432830a7d0434ac436aaa48f7c73eb28e2b9af53
Harshshrivastav <harshshrivastava096@gmail.com>
2024-08-12 12:58:36 +0530

    LSTM RNN v-2

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def predict_next_word(model, tokenizer, text, max_sequence_len):
 
 # streamlit app
 st.title("Next Word Prediction With LSTM And Early Stopping")
-input_text=st.text_input("Enter the sequence of Words","Your name ")
+input_text=st.text_input("Enter the sequence of Words","God bless ")
 if st.button("Predict Next Word"):
     max_sequence_len = model.input_shape[1] + 1  # Retrieve the max sequence length from the model input shape
     next_word = predict_next_word(model, tokenizer, input_text, max_sequence_len)
